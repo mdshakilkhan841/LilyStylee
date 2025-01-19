486794fe8c56bcebc36dbddc6d19af2df4f774b7
shakilkhan3676 <blc.apps2@daffodilvarsity.edu.bd>
2025-01-19 16:20:58 +0600

    Added Discount card

--- app/index.jsx
+++ app/index.jsx
@@ -1,17 +1,10 @@
-import {
-    ScrollView,
-    Text,
-    TouchableOpacity,
-    View,
-    Image,
-    Dimensions,
-} from "react-native";
+import { ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import TabBar from "../components/home/TabBar";
 import TopHeader from "../components/home/TopHeader";
 import Categories from "../components/home/Categories";
 import AdvertisementSlider from "../components/home/AdvertisementSlider";
-import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import DiscountCard from "../components/home/DiscountCard";
 
 export default function Index() {
     return (
@@ -21,27 +14,7 @@ export default function Index() {
             <ScrollView className="flex-1">
                 <Categories />
                 <AdvertisementSlider />
-                <View className="flex-row items-center justify-center flex-1 m-4 border border-pink-600 border-dashed divide-x-2 divide-pink-600 divide-dashed">
-                    <View className="items-center justify-center flex-1 w-1/2 p-1 border-r border-pink-600 border-dashed">
-                        <Text className="text-xl font-bold text-blue-800">
-                            EXTRA 10% OFF
-                        </Text>
-                        <Text className="text-sm">
-                            Use Code :{" "}
-                            <Text className="font-semibold">LILYSTYLEE</Text>
-                        </Text>
-                    </View>
-                    <View className="items-center justify-center flex-1 w-1/2 p-1">
-                        <Text className="text-sm font-bold text-center text-blue-800 ">
-                            GET FREE SHOPPING ON ALL ORDERS{" "}
-                            <FontAwesome6
-                                name="bag-shopping"
-                                size={18}
-                                color="#1e40af"
-                            />
-                        </Text>
-                    </View>
-                </View>
+                <DiscountCard />
             </ScrollView>
             <TabBar />
         </SafeAreaView>
