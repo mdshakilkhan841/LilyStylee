f36c09615c3d115aaf5917ab12371513bce8a341
shakilkhan3676 <blc.apps2@daffodilvarsity.edu.bd>
2025-01-20 19:24:55 +0600

    Modified product card and render with dummy data

--- components/AddToBagButton.jsx
+++ components/AddToBagButton.jsx
@@ -5,7 +5,7 @@ const AddToBagButton = () => {
     return (
         <TouchableOpacity
             activeOpacity={0.7}
-            className="mt-3 border border-pink-600 rounded-md"
+            className="mt-2 border border-pink-600 rounded-md"
         >
             <Text className="p-1.5 font-bold text-center text-pink-600 text-sm">
                 ADD TO BAG
