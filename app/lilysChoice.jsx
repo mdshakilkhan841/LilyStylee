5f4d6b964e017cb98aa9ba9d97b309896929bcf5
mdshakilkhan841 <blc.apps2@daffodilvarsity.edu.bd>
2025-05-17 17:52:34 +0600

    feat: implement Lily's Choice screen with Appbar and ScrollView

--- app/lilysChoice.jsx
+++ app/lilysChoice.jsx
@@ -1,11 +1,30 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Appbar, Badge, TouchableRipple } from "react-native-paper";
+import { router } from "expo-router";
 
 const lilysChoice = () => {
     return (
-        <View>
-            <Text>lilysChoice</Text>
-        </View>
+        <SafeAreaView edges={["bottom"]} className="flex-1">
+            <Appbar.Header style={{ backgroundColor: "white" }}>
+                <Appbar.BackAction
+                    onPress={() => {
+                        router.back();
+                    }}
+                />
+                <Appbar.Content
+                    title="Lily's Choice"
+                    titleStyle={{ fontSize: 16, fontWeight: "bold" }}
+                />
+            </Appbar.Header>
+            {/* Body */}
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+                // contentContainerStyle={{
+                //     paddingBottom: 8,
+                // }}
+            ></ScrollView>
+        </SafeAreaView>
     );
 };
 
