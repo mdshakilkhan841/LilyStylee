2db98a28d97534456f1e2e3fe9af7f41dc549b11
mdshakilkhan841 <blc.apps2@daffodilvarsity.edu.bd>
2025-05-17 16:38:57 +0600

    fix: update height calculation in AdvertisementSlider component for responsive design

--- components/home/AdvertisementSlider.jsx
+++ components/home/AdvertisementSlider.jsx
@@ -45,8 +45,8 @@ const AdvertisementSlider = () => {
                 loop
                 defaultIndex={0}
                 width={width}
+                height={width >= 768 ? 240 * 1.2 : 240 * 0.7}
                 // height={width / 3}
-                height={240 * 0.7}
                 autoPlay={true}
                 data={banners}
                 scrollAnimationDuration={2000}
