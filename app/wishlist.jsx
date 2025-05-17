606d6db501c1e085fa7fb4a12c6d2401fe681289
mdshakilkhan841 <blc.apps2@daffodilvarsity.edu.bd>
2025-05-17 17:52:40 +0600

    feat: implement wishlist screen with Appbar and ScrollView

--- app/wishlist.jsx
+++ app/wishlist.jsx
@@ -1,11 +1,54 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Appbar, Badge, TouchableRipple } from "react-native-paper";
+import { router } from "expo-router";
+import SimpleLineIcons from "@expo/vector-icons/SimpleLineIcons";
 
 const wishlist = () => {
     return (
-        <View>
-            <Text>wishlist</Text>
-        </View>
+        <SafeAreaView edges={["bottom"]} className="flex-1">
+            <Appbar.Header style={{ backgroundColor: "white" }}>
+                <Appbar.BackAction
+                    onPress={() => {
+                        router.back();
+                    }}
+                />
+                <Appbar.Content
+                    title="WISHLIST"
+                    titleStyle={{ fontSize: 16, fontWeight: "bold" }}
+                />
+                <TouchableRipple
+                    borderless={true}
+                    rippleColor="rgba(236, 72, 153, 0.15)"
+                    style={{
+                        borderRadius: 100,
+                        padding: 10,
+                    }}
+                    onPress={() => {}}
+                >
+                    <>
+                        <SimpleLineIcons name="bag" size={22} color="black" />
+                        <Badge
+                            style={{
+                                position: "absolute",
+                                top: 5,
+                                right: 1.5,
+                                backgroundColor: "#db2777",
+                            }}
+                        >
+                            30
+                        </Badge>
+                    </>
+                </TouchableRipple>
+            </Appbar.Header>
+            {/* Body */}
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+                // contentContainerStyle={{
+                //     paddingBottom: 8,
+                // }}
+            ></ScrollView>
+        </SafeAreaView>
     );
 };
 
